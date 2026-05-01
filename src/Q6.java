762a32803f6edda981ee493258d2a5349ecbaf03
GayanShyamal <gayanshyamal706@gmail.com>
2026-05-01 13:16:54 +0530

    done Q6

--- src/Q6.java
+++ src/Q6.java
@@ -1,5 +1,19 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Q6 {
     public static void main(String[] args) {
+
+        int year,age,temp = 2026;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your born year:");
+        year = scanner.nextInt();
+
+        age = temp - year;
+
+        System.out.println("You were born in "+year+" and will be (are) "+age+" this year.");
 
 
     }
