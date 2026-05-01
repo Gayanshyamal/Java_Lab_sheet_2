9ea73edb462e8e94c74b0aa9e802e8927bb4aa8e
GayanShyamal <gayanshyamal706@gmail.com>
2026-05-01 10:46:02 +0530

    done Q1 b

--- src/Q1_b.java
+++ src/Q1_b.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Q1_b {
+    public static void main(String[] args) {
+        int X,Y;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your X No:");
+        X = scanner.nextInt();
+
+        System.out.print("Enter your Y No:");
+        Y = scanner.nextInt();
+
+        System.out.println(X + (4*(Math.pow(Y,3))));
+    }
 }
